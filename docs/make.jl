e05a57a2e830c4ce0400728594193dd8e0c89ba1
Daniel Bergman <danielrbergman@gmail.com>
2026-04-14 16:56:21 -0400

    Add documentation files for ModelManager.jl

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, ModelManager
+
+DocMeta.setdocmeta!(ModelManager, :DocTestSetup, :(using ModelManager); recursive=true)
+
+
+makedocs(;
+    modules=[ModelManager],
+    authors="Daniel Bergman <danielrbergman@gmail.com> and contributors",
+    sitename="ModelManager.jl",
+    format=Documenter.HTML(;
+        canonical="https://drbergman-lab.github.io/ModelManager.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/drbergman-lab/ModelManager.jl",
+    devbranch="main",
+    push_preview=true,
+)
---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+# ModelManager.jl
+
+Generic infrastructure for managing simulation-based modeling projects.
+
+ModelManager.jl provides the base types, database management, variation handling,
+and runner infrastructure that simulator-specific packages (e.g.,
+[PhysiCellModelManager.jl](https://github.com/drbergman-lab/PhysiCellModelManager.jl))
+build on top of.
